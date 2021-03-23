1b9b9d31737129584e3a7a76eb3b2306c8d95431
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2021-03-23 12:59:00 +0100

    Aggiunta 2 form

---
+++ PersonaleWPF/Qualifiche.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonaleWPF
+{
+    /// <summary>
+    /// Logica di interazione per Qualifiche.xaml
+    /// </summary>
+    public partial class Qualifiche : Window
+    {
+        public Qualifiche()
+        {
+            InitializeComponent();
+        }
+    }
+}
